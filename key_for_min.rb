c5b539ec5671fea1c98af18dbda58e0f3b661297
John Million <jpmillion@comcast.net>
2020-11-11 15:37:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  min = name_hash.sum 
   name_hash.select do |key, value|
-    min = 0 
-    if value 
+    if value < min
+      min = value
+    end
+  end
+  return min
 end
